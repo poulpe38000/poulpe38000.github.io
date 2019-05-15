74623ff9683de111ac159d6473ae14e3792b27e5
poulpe38000 <32297400+poulpe38000@users.noreply.github.com>
2019-05-15 16:12:48 +0200

    initial commit for poetracker

---
+++ poetracker/precache-manifest.fa2ae6ee45e2676bb640dc442040c17b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3948f1e388acd0ee1f582c31327b870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a01e9f095e303668307",
+    "url": "/static/css/main.d2718870.chunk.css"
+  },
+  {
+    "revision": "b1906fe692d685588691",
+    "url": "/static/js/2.418aa8a4.chunk.js"
+  },
+  {
+    "revision": "1a01e9f095e303668307",
+    "url": "/static/js/main.251ae0c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
